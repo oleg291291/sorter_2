c662880b90d33a00b5d6945f81857294b249c904
oleg291291@mail.ru <oleg291291@mail.ru>
2018-02-25 20:34:46 +0300

    18-7

--- src/index.js
+++ src/index.js
@@ -1,30 +1,56 @@
 class Sorter {
   constructor() {
     // your implementation
+    this.arr = [];
+//this.comparator = compareNumeric;
+this.compareNumeric = function compareNumeric(a, b) {
+  if (a > b) return 1;
+  if (a < b) return -1;
+}
+
   }
 
   add(element) {
     // your implementation
+    this.arr.push(element);
   }
 
   at(index) {
     // your implementation
+    return this.arr[index];
   }
 
   get length() {
     // your implementation
+    return this.arr.length;
   }
 
   toArray() {
     // your implementation
+    return this.arr;
+
   }
 
   sort(indices) {
     // your implementation
+
+  var indicesSorted = indices.sort(this.compareNumeric);
+  var selected = [];
+   for(var i = 0; i<indicesSorted.length; i++){
+     selected.push(this.arr[indicesSorted[i]]);
+   }
+   var sorted = selected.sort(this.compareNumeric);
+   for(var k = 0; k<sorted.length; k++){
+     this.arr[indicesSorted[k]] = sorted[k];
+   }
+
+
+
   }
 
   setComparator(compareFunction) {
     // your implementation
+
   }
 }
 
